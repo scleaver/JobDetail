95bf4811c1d11c9232077bf9205c1aed13f04856
Seth Cleaver <seth@cleaver.consulting>
2023-10-02 13:52:00 +1000

    Error handling

--- FRPlusComponents/src/services/JobService.ts
+++ FRPlusComponents/src/services/JobService.ts
@@ -1,17 +1,21 @@
-import axios from 'axios';
-import type { Job } from "@/types/Job";
+import axios from 'axios'
+import type { Job } from '@/types/Job'
 
 export default class JobService {
-    static async getJob(dataId: string): Promise<Job | null> {
-      try {
-        const response = await axios.get(`https://local.fr5.com.au/internal/jobs/${dataId}`);
-        return response.data as Job;
-      } catch (error: any) {
+  static async getJob(dataId: string): Promise<Job | null> {
+    try {
+      const response = await axios.get(`https://local.fr5.com.au/internal/jobs/${dataId}`)
+      return response.data as Job
+    } catch (error: any) {
+      // You can use 'any' here for simplicity
+      if (axios.isAxiosError(error)) {
+        // Type guard
         if (error.response && error.response.status === 404) {
-          return null;
+          return null
         }
-        console.error('An error occurred:', error);
-        return null;
       }
+      console.error('An error occurred:', error)
+      return null
     }
   }
+}
