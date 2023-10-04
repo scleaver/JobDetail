bfec66455013e3436d554047dcdd570d980d9ddf
Seth Cleaver <seth@cleaver.consulting>
2023-10-04 13:41:20 +1000

    Latest

--- FRPlusComponents/src/components/JobDetail/CertificatePreview.vue
+++ FRPlusComponents/src/components/JobDetail/CertificatePreview.vue
@@ -7,57 +7,109 @@
             </div>
         </div>
         <div class="card-body p-0 h-100">
-            <div ref="certView" class="h-100"></div>
+            {{ adobeApiReady }}
             <div v-if="job.calculationStatus !== 7">
                 Loading: {{ job.calculationStatusName }}
             </div>
+            <div v-else ref="certContainer" class="h-100"></div>
         </div>
     </div>
 </template>
   
 <script lang="ts">
-import { ref, defineComponent, type PropType, onMounted } from 'vue';
+import { ref, defineComponent, watch, type PropType, onMounted } from 'vue';
 import type { Job } from '@/types/Job';
 
 export default defineComponent({
-  props: {
-    job: {
-      type: Object as PropType<Job>,
-      required: true,
+    props: {
+        job: {
+            type: Object as PropType<Job>,
+            required: true,
+        },
     },
-  },
-  setup(props) {
-    const adobeApiReady = ref(false);
-    const previewFilePromise = ref(null);
+    setup(props) {
+        const adobeApiReady = ref(false);
+        const previewFilePromise = ref(null);
+        const certContainer = ref<HTMLElement | null>(null);
 
-    const loadAdobeSdk = () => {
-      return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = 'https://documentservices.adobe.com/view-sdk/viewer.js';
-        script.onload = resolve;
-        script.onerror = reject;
-        document.head.appendChild(script);
-      });
-    };
+        const loadAdobeSdk = () => {
+            return new Promise((resolve, reject) => {
+                const script = document.createElement('script');
+                script.src = 'https://documentservices.adobe.com/view-sdk/viewer.js';
+                script.onload = resolve;
+                script.onerror = reject;
+                document.head.appendChild(script);
+            });
+        };
 
-    onMounted(async () => {
-      await loadAdobeSdk();
-      document.addEventListener("adobe_dc_view_sdk.ready", () => {
-        adobeApiReady.value = true;
-        const adobeDCView = new AdobeDC.View({ clientId: "9b98e1fecda342f38c1d6c8568021afe", divId: "cert-view" });
-        if (props.job.calculationStatus === 7 && props.job.previewUrl) {
-          adobeDCView.previewFile({
-            content: { location: { url: props.job.previewUrl } },
-            metaData: { fileName: props.job.previewFilename }
-          }, { embedMode: "SIZED_CONTAINER" });
-        }
-      });
-    });
+        const renderPdf = (url: string, fileName: string) => {
+            if (!adobeApiReady.value) return;
 
-    return {
-      adobeApiReady,
-      previewFilePromise,
-    };
-  }
+            const previewConfig = {
+                defaultViewMode: 'FIT_WIDTH',
+                showAnnotationTools: false,
+            };
+
+            if (certContainer.value) {
+                certContainer.value.innerHTML = '';
+            }
+
+            const viewer = document.createElement('div');
+            viewer.id = 'viewer';
+
+            if (certContainer.value) {
+                certContainer.value.appendChild(viewer);
+            }
+
+            const adobeDCView = new AdobeDC.View({
+                clientId: '9b98e1fecda342f38c1d6c8568021afe',
+                divId: 'viewer',
+            });
+
+            previewFilePromise.value = adobeDCView.previewFile(
+                {
+                    content: {
+                        location: {
+                            url: url,
+                        },
+                    },
+                    metaData: {
+                        fileName: fileName,
+                        id: fileName,
+                    },
+                },
+                previewConfig
+            );
+        };
+
+        onMounted(async () => {
+            await loadAdobeSdk();
+            document.addEventListener('adobe_dc_view_sdk.ready', () => {
+                adobeApiReady.value = true;
+                if (props.job.previewUrl && props.job.calculationStatus == 7) {
+                    const fullPreviewUrl = `https://local.fr5.com.au${props.job.previewUrl}`;
+                    renderPdf(fullPreviewUrl, props.job.previewFilename);
+                }
+            })
+        });
+
+        watch(
+            () => [props.job.previewUrl, props.job.calculationStatus, props.job.previewFilename],
+            ([previewUrl, calculationStatus, previewFilename]) => {
+                console.log("loading");
+                if (previewUrl && calculationStatus == 7 && previewFilename) {
+                    const fullPreviewUrl = `https://local.fr5.com.au${previewUrl}`;
+                    renderPdf(fullPreviewUrl, previewFilename);
+                }
+            }
+        );
+
+        return {
+            adobeApiReady,
+            previewFilePromise,
+            certContainer,
+            renderPdf,
+        };
+    }
 });
 </script>
